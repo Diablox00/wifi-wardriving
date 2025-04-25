dc4c59f2e035a4cb8e72d10c60712f15be84d448
Diablo <96569494+Diablox00@users.noreply.github.com>
2025-04-25 09:52:32 +0500

    Create google-earth.py

---
+++ google-earth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import webbrowser
+
+url = "https://earth.google.com/web/"
+
+# Choose a specific browser - uncomment the one you want
+# webbrowser.get('firefox').open(url)
+# webbrowser.get('google-chrome').open(url)
+# webbrowser.get('chromium').open(url)
+
+# Or just use the default browser
+webbrowser.open(url)
